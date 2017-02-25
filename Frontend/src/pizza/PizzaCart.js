f8b6fa24563c55e0f09bc4f9ff923017f9be443b
naumchyk <andriy.nau@gmail.com>
2017-02-25 14:53:49 +0200

    Додана певна логіка
    тепер можна працювати з бай-лістом

--- Frontend/src/pizza/PizzaCart.js
+++ Frontend/src/pizza/PizzaCart.js
@@ -9,31 +9,41 @@ var PizzaSize = {
     Small: "small_size"
 };
 
+
+
 //Змінна в якій зберігаються перелік піц в кошику
 var Cart = [];
 
 //HTML едемент куди будуть додаватися піци
 var $cart = $("#cart");
 
+
 function addToCart(pizza, size) {
-    //Додавання однієї піци в кошик покупок
-
-    //Приклад реалізації, можна робити будь-яким іншим способом
-    Cart.push({
-        pizza: pizza,
-        size: size,
-        quantity: 1
-    });
-
-    //Оновити вміст кошика на сторінці
+    var n=-1;
+    for(var i = 0;i<Cart.length;i++)
+       if((Cart[i].pizza==pizza)&&(Cart[i].size==size))
+           n=i;
+   if(n!=-1)
+        Cart[n].quantity++;
+   else{
+        Cart.push({
+            pizza: pizza,
+            size: size,
+            quantity: 1
+        });
+   }
     updateCart();
 }
 
-function removeFromCart(cart_item) {
-    //Видалити піцу з кошика
-    //TODO: треба зробити
 
-    //Після видалення оновити відображення
+
+function removeFromCart(cart_item) {
+    data = [];
+    for(var i = 0;i<Cart.length;i++){
+        if((Cart[i].pizza!=cart_item.pizza)||(Cart[i].size!=cart_item.size))
+            data.push(Cart[i]);
+    }
+    Cart = data;
     updateCart();
 }
 
@@ -41,9 +51,14 @@ function initialiseCart() {
     //Фукнція віпрацьвуватиме при завантаженні сторінки
     //Тут можна наприклад, зчитати вміст корзини який збережено в Local Storage то показати його
     //TODO: ...
-
     updateCart();
 }
+
+$(".clear-list").click(function () {
+    Cart = [];
+    updateCart();
+})
+
 
 function getPizzaInCart() {
     //Повертає піци які зберігаються в кошику
@@ -71,10 +86,19 @@ function updateCart() {
             updateCart();
         });
 
+        $node.find(".minus").click(function () {
+            cart_item.quantity--;
+           if (cart_item.quantity==0) removeFromCart(cart_item);
+            updateCart();
+        })
+
         $cart.append($node);
     }
 
     Cart.forEach(showOnePizzaInCart);
+
+    if(Cart.length==0)
+        $cart.html(" Пусто в холодильнику? <br> Замовте піцу!");
 
 }
 
